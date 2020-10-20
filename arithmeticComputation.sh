9fa6c2425f316779a13a061d80cad9f1386b8592
HimaBindhu-Madhamanchi <himabindhumadhamanchi@gmail.com>
2020-10-20 10:17:27 +0530

    Program to perform c+a/b

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -3,5 +3,5 @@ echo "Welcome to arthmetic operations"
 read -p "Enter number 1 :" a
 read -p "Enter number 2 :" b
 read -p "Enter number 3 :" c
-computation2=$(( $a*$b+$c ))
-echo "second computation is:" $computation2
+computation3=$(( $c+$a/$b ))
+echo "Third computation is:" $computation3
