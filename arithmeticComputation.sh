3025e2909b7c88c070f4276eb4660fc29e3fde5f
HimaBindhu-Madhamanchi <himabindhumadhamanchi@gmail.com>
2020-10-20 10:34:54 +0530

    Store the results in dictionary for every computation

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -3,5 +3,22 @@ echo "Welcome to arthmetic operations"
 read -p "Enter number 1 :" a
 read -p "Enter number 2 :" b
 read -p "Enter number 3 :" c
+
+computation1=$(( $a+$b*$c ))
+echo "first computation is:" $computation1
+
+computation2=$(( $a*$b+$c ))
+echo "second Computation is:"$computation2
+
+computation3=$(( $c+$a/$b ))
+echo "third computation is:"$computation3
+
 computation4=$(( $a%$b+$c ))
 echo "fourth computation is:" $computation4
+
+declare -A computations
+computation[1]=$computation1
+computation[2]=$computation2
+computation[3]=$computation3
+computation[4]=$computation4
+echo "dictionary values are :" ${computation[@]}
