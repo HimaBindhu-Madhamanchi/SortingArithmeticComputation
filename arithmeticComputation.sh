df9e95ba978acacca2eb667db26c6c79a244a8ea
HimaBindhu-Madhamanchi <himabindhumadhamanchi@gmail.com>
2020-10-30 12:14:26 +0530

    added a program to compute Arithmetic operation3

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -2,6 +2,8 @@
 echo "Welcome to arthmetic operations"
 read -p "Enter number 1 :" a
 read -p "Enter number 2 :" b
+
 read -p "Enter number 3 :" c
+
 computation3=$(( $c+$a/$b ))
 echo "Third computation is:" $computation3
