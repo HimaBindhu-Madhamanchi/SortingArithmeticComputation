148df4dc96b46ea0ff23bfbbf3bc46ae6ad61fe6
HimaBindhu-Madhamanchi <himabindhumadhamanchi@gmail.com>
2020-10-30 12:17:26 +0530

    added a program to compute Arithmetic operation4

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -3,5 +3,6 @@ echo "Welcome to arthmetic operations"
 read -p "Enter number 1 :" a
 read -p "Enter number 2 :" b
 read -p "Enter number 3 :" c
+
 computation4=$(( $a%$b+$c ))
 echo "fourth computation is:" $computation4
